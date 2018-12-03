c7434949114e903640dbb6fb2ae407ba625e0e9c
unknown <nbatre1@yahoo.com>
2018-12-03 00:21:19 -0600

    keep moving forward

--- weight.js
+++ weight.js
@@ -6,6 +6,9 @@ document.getElementById('lbsInput').addEventListener('input', function(e){
 
     document.getElementById('gramsOutput').innerHTML= ML = lbs/0.0022046;
 
+    document.getElementById('kgOutput').innerHTML = KL = lbs/2.205;
+
+
     document.getElementById('ouncesOutput').innerHTML = TL = lbs*16;
 });
 
