586feb6855e581db0ff81d244941930679389129
unknown <nbatre1@yahoo.com>
2018-12-03 00:30:02 -0600

    keep moving forward

--- weight.js
+++ weight.js
@@ -2,6 +2,7 @@
  document.getElementById('output').style.visibility ='hidden';
 
 document.getElementById('lbsInput').addEventListener('input', function(e){
+    document.getElementById('output').style.visibility ='visible';
     let lbs = e.target.value;
     console.log(lbs);
     console.log("123");
