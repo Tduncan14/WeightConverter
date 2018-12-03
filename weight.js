cacf13adf9ab032f78555afd2a75df68f85ac287
unknown <nbatre1@yahoo.com>
2018-12-03 00:23:12 -0600

    keep moving forward

--- weight.js
+++ weight.js
@@ -4,12 +4,12 @@ document.getElementById('lbsInput').addEventListener('input', function(e){
     console.log(lbs);
     console.log("123");
 
-    document.getElementById('gramsOutput').innerHTML= ML = lbs/0.0022046;
+    document.getElementById('gramsOutput').innerHTML= ML = lbs/0.0022046 +  " grams";
 
-    document.getElementById('kgOutput').innerHTML = KL = lbs/2.205;
+    document.getElementById('kgOutput').innerHTML = KL = lbs/2.205 + " kilograms";
 
 
-    document.getElementById('ouncesOutput').innerHTML = TL = lbs*16;
+    document.getElementById('ouncesOutput').innerHTML = TL = lbs*16 + " ounces";
 });
 
 
